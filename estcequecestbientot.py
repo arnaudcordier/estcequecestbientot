00b216b7d560d8074cc6b1fa2f740d07789cc277
ArnAud Cordier <arnaud@mutins.net>
2014-08-31 12:24:47 +0200

    Documentation: add similar sites and projects

--- estcequecestbientot.py
+++ estcequecestbientot.py
@@ -32,8 +32,19 @@ if __name__ == '__main__':
 
 
 '''
-Similar project (PHP)
-https://github.com/tontonDuPirox/estcequecestbientot
+Similar projects
+ https://github.com/tontonDuPirox/estcequecestbientot (php)
+ https://github.com/hughevans/isitcoffeetime.com (ruby)
+ https://github.com/isitchristmas (js)
+ https://github.com/ravinggenius/weekend (js)
+ https://github.com/attaboy/isitteatimeyet (php)
+ https://github.com/antoinefriteau/estcequecestlheureducafe (js)
+ https://github.com/chregu/isitfullmoon (php)
+ https://github.com/antoinefriteau/estcequecestlheuredalleraucinema.fr (php)
+ https://github.com/noahd1/isitburgerwednesday.com (js)
+ https://github.com/kd35a/isitthefuture (php)
+ https://github.com/jingweno/isittheday.com (ruby)
+ https://github.com/stilldavid/isittimetogohome.com (php)
 
 purpose
  display a message depending of the time of the day
@@ -44,4 +55,26 @@ synopsis
  get the current date/time
  find the message corresponding to the current time
  display the message
+ 
+sites
+ http://estcequecestbientot.fr/
+  http://estcequecestbientotlheuredemanger.fr/
+  http://estcequecestbientotlapero.fr/
+  http://estcequecestbientotlhappyhours.fr/
+  http://cestquandlessoldes.com/
+  http://estcequecestbientotlesvacances.fr/
+  http://estcequecestbientotleweekend.fr/
+  http://estcequecestbientotlematch.fr/
+  http://estcequecestbientotnoel.fr/
+  http://estcequecestbientotlete.fr/
+  http://estcequecestbientotlafriteduvendredi.fr/
+ http://www.estcequonmetenprodaujourdhui.info/
+ http://www.isittheweekendyet.com/
+ http://isittheweekendyet.co.uk/
+ http://isittimeforabeer.com/
+ http://isittimeforbed.com/
+ https://isitchristmas.com/
+ http://isitteatimeyet.com/
+ http://isitcoffeetime.com/
+ http://isitfullmoon.com
 '''
