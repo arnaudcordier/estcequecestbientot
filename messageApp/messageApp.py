87a1447938390e13071767a57ce90987ae00c83a
ArnAud Cordier <arnaud@mutins.net>
2014-09-15 22:53:34 +0200

    Implémentation de messageApp.listMessages()
    Recevoir la liste des messages chargés et de ceux non-chargés

--- messageApp/messageApp.py
+++ messageApp/messageApp.py
@@ -1,38 +1,44 @@
 """
  There should be a insightful documentation here
 """
+import glob
 import os.path
-
 from datetime import datetime
 from collections import OrderedDict
 from messageApp.messages import Messages
 
 class MessageApp():
-	def __init__(self):
+	def __init__(self, messagesDir='messages', filePattern="messages_"):
+		self.filePattern = os.path.join(messagesDir, filePattern)
 		self.messages = OrderedDict()
 		
 	def loadMessage(self, names):
 		if type(names) is str:
-			names = names,
+			names = (names, )
 		
+		fileList = self._getFileList()
 		for name in names:
-			if (not name in self.messages):
-				filename = 'messages/messages_' + name + '.yaml'
-				if os.path.isfile(filename):
-					self.messages[name] = Messages(filename)
+			if (name not in self.messages) and (name in fileList):
+				filename = self.filePattern + name + '.yaml'
+				self.messages[name] = Messages(filename)
 			else:
 				self.messages[name].reload()
 
 	def unloadMessage(self, names):
 		if type(names) is str:
-			names = names,
+			names = (names, )
 		
 		for name in names:
 			if (name in self.messages):
 				self.messages.pop(name)
 
+	def listMessages(self):
+		loadedList = [ name for name, _ in self.messages.items() ]
+		notloadedList = list(set(self._getFileList()) - set(loadedList))
+		return notloadedList, loadedList
+
 	def reload(self):
-		for name, messages in self.messages.items():
+		for _, messages in self.messages.items():
 			messages.reload()
 
 	def getMessages(self):
@@ -41,8 +47,14 @@ class MessageApp():
 
 	def getMessagesAtTime(self, time):
 		messageList = []
-		for name, messages in self.messages.items():
+		for _, messages in self.messages.items():
 			message = messages.getMessage(time)
 			if message:
 				messageList.append(message)
 		return messageList
+
+	def _getFileList(self):
+		fileList = glob.glob(self.filePattern + "*.yaml")
+		nameStartAt = len(self.filePattern)
+		fileList = [ name[nameStartAt:-5] for name in fileList ]
+		return fileList
