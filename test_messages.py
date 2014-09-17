dc0a8d466e5b46a4f050c49623b95378f2e28b2e
ArnAud Cordier <arnaud@arnaudcordier.org>
2014-09-17 03:23:45 +0200

    Plus jolis noms pour les fonctions de test

--- test_messages.py
+++ test_messages.py
@@ -3,29 +3,29 @@ from messageApp.intervalBit import IntervalBit
 
 ib = IntervalBit('5-45,52-56,57', 6)
 
-def test_bitFit_interval1():
+def test_IntervalBit_doesItFit_1():
 	assert ib.doesItFit(6) == [5, 45]
 
-def test_bitFit_interval2():
+def test_IntervalBit_doesItFit_2():
 	assert ib.doesItFit(55) == [52,56]
 
-def test_bitFit_interval3():
+def test_IntervalBit_doesItFit_3():
 	assert ib.doesItFit(52) == [52,56]
 
-def test_bitFit_interval4():
+def test_IntervalBit_doesItFit_4():
 	assert ib.doesItFit(56) == [52,56]
 
-def test_bitFit_exact():
+def test_IntervalBit_doesItFit_exact():
 	assert ib.doesItFit(57) == [57,57]
 
-def test_bitFit_none():
+def test_IntervalBit_doesItFit_none():
 	assert ib.doesItFit(4) == None
 
-def test_bitFit_all():
+def test_IntervalBit_doesItFit_all():
 	a = IntervalBit('5-45,52-56,57,*', 6)
 	assert a.doesItFit(46) == ['*','*']
 
-def test_bitFit_eliminate_out_of_boundary():
+def test_IntervalBit_doesItFit_no_out_of_boundary():
 	a = IntervalBit('52-56,57,89-95', 6)
 	assert a._intervals == [[52,56], [57,57]]
 
@@ -38,23 +38,23 @@ interval= '* * 13-22 5-7 09-20,23 *'
 messages = [('10:30', 'message1'), ('11:45', 'message2'), ('13:12', 'message3'), ('09:12', 'message4')]
 i = Interval(interval, messages)
 
-def test_interval_fit_1():
+def test_Interval_doesItFit_1():
 	assert i._doesItFit([2013, 12, 13, 5, 15, 12]) == [['*', '*'], ['*', '*'], [13, 22], [5, 7], [9, 20], ['*', '*']]
 
-def test_interval_nofit():
-	assert i._doesItFit([2013, 12, 13, 5,  2, 18]) == None
-
-def test_interval_fit_2():
+def test_Interval_doesItFit_2():
 	assert i._doesItFit([2013, 12, 13, 5, 23, 53]) == [['*', '*'], ['*', '*'], [13, 22], [5, 7], [23, 23], ['*', '*']]
 
-def test_interval_message_1():
+def test_Interval_doesItFit_not():
+	assert i._doesItFit([2013, 12, 13, 5,  2, 18]) == None
+
+def test_Interval_getMessage_1():
 	assert i.getMessage(datetime(2013, 12, 13, 23, 53)) == 'message3'
 
-def test_interval_message_2():
-	assert i.getMessage(datetime(2013, 12, 13, 9, 11)) == None
-
-def test_interval_message_3():
+def test_Interval_getMessage_2():
 	assert i.getMessage(datetime(2013, 12, 13, 9, 12)) == 'message4'
+
+def test_Interval_getMessage_not():
+	assert i.getMessage(datetime(2013, 12, 13, 9, 11)) == None
 
 
 # Test Messages
@@ -70,28 +70,27 @@ def test_Messages_getMessage():
 	print(estcequecestbientot)
 	assert estcequecestbientot == ('montitre', 'message3')
 
-
 # test MessageApp
 from messageApp.messageApp import MessageApp
 from datetime import datetime
 
 m = MessageApp('messages', "messages_")
 
-def test_loadMessages():
+def test_MessageApp_loadMessages():
 	m.loadMessage('object')
 	_, l = m.listMessages()
 	print(l)
 	assert l == ['object']
 
-def test_unloadMessage():
+def test_MessageApp_unloadMessage():
 	m.unloadMessage('object')
 	_, l = m.listMessages()
 	print(l)
 	assert l == []
-	
+
 # def test_listMessages() # allready done
 
-def test_reload(): # does not realy test reload of file
+def test_MessageApp_reload(): # does not realy test reload of file
 	m.loadMessage('object')
 	_, l1 = m.listMessages()
 	m.reload()
@@ -100,10 +99,9 @@ def test_reload(): # does not realy test reload of file
 
 #def test_getMessages(): # too tricky
 
-def test_getMessagesAtTime():
+def test_MessageApp_getMessagesAtTime():
 	m.loadMessage('object')
 	estcequecestbientot = m.getMessagesAtTime(datetime(2013, 12, 13, 23, 53))
 	print(estcequecestbientot)
 	assert estcequecestbientot == [('montitre', 'message3')]
-
 
